4105d002bec7b4c1d20777f20bd7bfbafbdd5367
bugname7 <valisher2106@gmail.com>
2024-12-01 11:12:13 +0500

    create films

--- src/App.jsx
+++ src/App.jsx
@@ -1,72 +1,4 @@
-// import React, { useEffect, useState } from "react";
-// import "./App.css";
-// import axios from "axios";
-
-// function App() {
-//   const [name, setName] = useState("");
-//   const [user, setUser] = useState([]);
-//   const [filterUser, setFilterUser] = useState([]);
-
-//   useEffect(() => {
-//     axios
-//       .get("https://jsonplaceholder.typicode.com/posts")
-//       .then((response) => {
-//         if (response.status === 200) {
-//           setUser(response.data);
-//           setFilterUser(response.data);
-//         }
-//       })
-//       .catch((err) => {
-//         console.log(err);
-//       });
-//   }, []);
-
-//   useEffect(() => {
-//     if (name === "") {
-//       setFilterUser(user);
-//     } else {
-//       setFilterUser(
-//         user.filter((users) =>
-//           users.title.toLowerCase().includes(name.toLowerCase())
-//         )
-//       );
-//     }
-//   }, [name, user]);
-
-//   return (
-//     <div>
-//       <div className="container">
-//         <div className="workSpace">
-//           <h2>Ish qidiruv formasi</h2>
-//           <form className="work ">
-//             <input
-//               type="text"
-//               value={name}
-//               onChange={(e) => setName(e.target.value)}
-//               placeholder="ish nomi....."
-//             />
-//           </form>
-//         </div>
-//         <div className="cards-title">
-//           {filterUser.length > 0 ? (
-//             filterUser.map((users, index) => (
-//               <div className="works-title" key={users.id}>
-//                 <h3>Title: {users.title}</h3>
-//                 <h5>Body: {users.body}</h5>
-//               </div>
-//             ))
-//           ) : (
-//             <p>error</p>
-//           )}
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-// import React, { useEffect, useState } from "react";
+// // import React, { useEffect, useState } from "react";
 // import axios from "axios";
 // import "./App.css";
 
@@ -315,6 +247,158 @@ import React, { useState, useEffect } from "react";
 //             <p>Shamol tezligi: {cityWeather.wind.speed}m/s</p>
 //           </div>
 //         ))}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React, { useEffect, useState } from "react";
+// import "./App.css";
+// import axios from "axios";
+
+// function App() {
+//   const [name, setName] = useState("");
+//   const [user, setUser] = useState([]);
+//   const [filterUser, setFilterUser] = useState([]);
+
+//   useEffect(() => {
+//     axios
+//       .get("https://jsonplaceholder.typicode.com/posts")
+//       .then((response) => {
+//         if (response.status === 200) {
+//           setUser(response.data);
+//           setFilterUser(response.data);
+//         }
+//       })
+//       .catch((err) => {
+//         console.log(err);
+//       });
+//   }, []);
+
+//   useEffect(() => {
+//     if (name === "") {
+//       setFilterUser(user);
+//     } else {
+//       setFilterUser(
+//         user.filter((users) =>
+//           users.title.toLowerCase().includes(name.toLowerCase())
+//         )
+//       );
+//     }
+//   }, [name, user]);
+
+//   return (
+//     <div>
+//       <div className="container">
+//         <div className="workSpace">
+//           <h2>Ish qidiruv formasi</h2>
+//           <form className="work ">
+//             <input
+//               type="text"
+//               value={name}
+//               onChange={(e) => setName(e.target.value)}
+//               placeholder="ish nomi....."
+//             />
+//           </form>
+//         </div>
+//         <div className="cards-title">
+//           {filterUser.length > 0 ? (
+//             filterUser.map((users, index) => (
+//               <div className="works-title" key={users.id}>
+//                 <h3>Title: {users.title}</h3>
+//                 <h5>Body: {users.body}</h5>
+//               </div>
+//             ))
+//           ) : (
+//             <p>error</p>
+//           )}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React, { useState } from "react";
+// import "./App.css";
+// import axios from "axios";
+
+// function App() {
+//   const [films, setFilms] = useState([]);
+//   const [loading, setLoading] = useState(false);
+//   const [name, setName] = useState("");
+//   const api_key = "4ba1bb5e";
+//   function validate() {
+//     if (name.length < 1) {
+//       alert("kino nomi juda qisqa");
+//       return false;
+//     }
+//     return true;
+//   }
+//   const handleSearch = (e) => {
+//     e.preventDefault();
+//     const isvalid = validate();
+//     if (!isvalid) {
+//       return;
+//     }
+//     if (name.trim() === "") return;
+//     setLoading(true);
+//     axios
+//       .get(`http://www.omdbapi.com/?apikey=${api_key}&s=${name}`)
+//       .then((response) => {
+//         if (response.status === 200) {
+//           setFilms(response.data.Search || []);
+//           setLoading(false);
+//         }
+//       })
+//       .catch((error) => {
+//         console.log(error);
+//         setLoading(false);
+//       });
+//   };
+
+//   return (
+//     <div className="app">
+//       <div className="container">
+//         <h2>Galivud Kinolarini Qidirish</h2>
+//         <form className="search-form">
+//           <input
+//             type="text"
+//             value={name}
+//             onChange={(e) => setName(e.target.value)}
+//             placeholder="Kino nomini kiriting"
+
+//           />
+//           <br />
+//           <button onClick={handleSearch} className="search-button">
+//             Qidirish
+//           </button>
+//         </form>
+//       </div>
+
+//       <div className="container">
+//         {loading && <p>ozgina kutib turing...</p>}
+//         {films.length > 0 ? (
+//           <div className="films-list">
+//             {films.map((film, index) => (
+//               <div key={index} className="film-card">
+//                 <h3>title:{film.Title}</h3>
+//                 <h4>year:{film.Year}</h4>
+//                 <img
+//                   width="200px"
+//                   src={film.Poster !== "N/A" ? film.Poster : "/placeholder.jpg"}
+//                   alt={film.Title}
+//                   className="film-poster"
+//                 />
+//               </div>
+//             ))}
+//           </div>
+//         ) : (
+//           !loading && <p>boshqa kino qidirib koring</p>
+//         )}
 //       </div>
 //     </div>
 //   );
